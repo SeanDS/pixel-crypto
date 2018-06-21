97a670c06f7136bdff5354441c74ebcbc16d1b6b
Sean Leavey <gpg@attackllama.com>
2018-06-21 09:34:53 +0200

    Update

--- pixel.py
+++ pixel.py
@@ -1,5 +1,9 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageOps, ImageDraw, ImageFont
 import random
+
+# colours
+black = (0, 0, 0)
+white = (255, 255, 255)
 
 def add_mark(pixels, x, y, marker_height, marker_half_width):
     # draw down rows
@@ -15,44 +19,49 @@ def draw_text(draw, x, y, text, font):
     # draw centred around x and y
     draw.text((x - w // 2, y - h // 2), text, font=font)
 
-def encipher(img, n, seed):
+def encipher(img, x_buf, seed):
     random.seed(seed)
 
-    # copy image n times
-    layers = [img.copy() for _ in range(n)]
-    layer_pixels = [layer.load() for layer in layers]
+    # original image pixels
+    pixels_base = img.load()
 
-    pixels = img.load()
+    # new layer
+    img_rand = Image.new("RGB", (img.size[0], img.size[1]))
+    pixels_rand = img_rand.load()
 
-    # colours
-    black = (0, 0, 0)
-    white = (255, 255, 255)
+    # generate random noise layer
+    add_noise(img_rand, x_buf)
 
-    def set_layer_colours(colours):
-        for layer, colour in zip(layer_pixels, colours):
-            layer[i, j] = colour
+    # create inverse layer
+    img_rand_inv = img_rand.copy()
+    pixels_rand_inv = img_rand_inv.load()
 
     # loop over pixels
-    for i in range(img.size[0]):
-        for j in range(img.size[1]):
-            # if pixel is white, all layers must be white
-            if pixels[i, j] == white:
-                set_layer_colours([white for _ in range(n)])
-            else:
-                # at least one layer must be black
-                colours = [black] + [white] * (n - 1)
-
-                # shuffle colours
-                random.shuffle(colours)
-
-                # set colours
-                set_layer_colours(colours)
+    for i in range(x_buf, img_rand.size[0] - x_buf):
+        for j in range(img_rand.size[1]):
+            # if pixel is white, don't invert
+            if pixels_base[i, j] == white:
+               pixels_rand_inv[i, j] = pixels_rand[i, j]
+            else:        
+                # compute inverse
+                if pixels_rand[i, j] == white:
+                    pixels_rand_inv[i, j] = black
+                else:
+                    pixels_rand_inv[i, j] = white
     
-    return layers
+    return [img_rand, img_rand_inv]
+
+def add_noise(image, x_buf):
+    pixels = image.load()
+
+    for i in range(x_buf, image.size[0] - x_buf):
+        for j in range(image.size[1]):
+            # random pixel
+            pixels[i, j] = random.choice([black, white])
 
 if __name__ == "__main__":
     # total size
-    x = 2050
+    x = 2250
     y = 300
 
     # ruler size
@@ -70,9 +79,15 @@ if __name__ == "__main__":
     small_marker_half_width = 1
     small_marker_spacing = 10
 
+    fidutial_1_x = 50
+    fidutial_1_y = 150
+    fidutial_2_x = x - 50
+    fidutial_2_y = 150
+    fidutial_r = 15
+
     font_path = "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf"
-    font_size = 25
-    text_height_offset = 15
+    font_size = 60
+    text_height_offset = 30
 
     # black image
     # Coordinates: (columns, rows)
@@ -106,10 +121,38 @@ if __name__ == "__main__":
             # add small mark
             add_mark(pixels, x_buf + i, img.size[1], small_marker_height, small_marker_half_width)
 
-    # create cyphertexts
-    layers = encipher(img, 2, seed=25526)
+    # create cyphers
+    layers = encipher(img, x_buf, seed=25526)
 
-    img.show()
+    # add fidutials
+    draw = ImageDraw.Draw(layers[0])
+    draw.ellipse((fidutial_1_x - fidutial_r, fidutial_1_y - fidutial_r, fidutial_1_x + fidutial_r, fidutial_1_y + fidutial_r), fill=(255, 255, 255))
+    draw.ellipse((fidutial_2_x - fidutial_r, fidutial_2_y - fidutial_r, fidutial_2_x + fidutial_r, fidutial_2_y + fidutial_r), fill=(255, 255, 255))
+    draw = ImageDraw.Draw(layers[1])
+    draw.ellipse((fidutial_1_x - fidutial_r, fidutial_1_y - fidutial_r, fidutial_1_x + fidutial_r, fidutial_1_y + fidutial_r), fill=(255, 255, 255))
+    draw.ellipse((fidutial_2_x - fidutial_r, fidutial_2_y - fidutial_r, fidutial_2_x + fidutial_r, fidutial_2_y + fidutial_r), fill=(255, 255, 255))
+
+    #img.show()
+    #layers[0].show()
+    #layers[1].show()
     
-    for layer in layers:
-        layer.show()
+    # add borders
+    layers[0] = ImageOps.expand(layers[0], border=20, fill='black')
+    layers[1] = ImageOps.expand(layers[1], border=20, fill='black')
+
+    # save
+    layers[0].save("layer1.png")
+    layers[1].save("layer2.png")
+
+    # create A4 sheet
+    img_a4 = Image.new("RGBA", (2100, 2970), (0, 0, 0, 0))
+
+    # paste
+    img_a4.paste(layers[0].rotate(90, expand=1), (15, 360))
+    img_a4.paste(layers[1].rotate(90, expand=1), (360, 360))
+    img_a4.paste(layers[0].rotate(90, expand=1), (705, 360))
+    img_a4.paste(layers[1].rotate(90, expand=1), (1050, 360))
+    img_a4.paste(layers[0].rotate(90, expand=1), (1395, 360))
+    img_a4.paste(layers[1].rotate(90, expand=1), (1740, 360))
+
+    img_a4.save("a4.png")
